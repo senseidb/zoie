181e1d4b465fd99b68f5c74280ae6507dac344f3
John Wang <john.wang@gmail.com>
2011-03-01 17:21:20 -0800

    removed zoie admin

--- zoie-admin/src/proj/zoie/admin/client/search/SearchServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package proj.zoie.admin.client.search;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface SearchServiceAsync {
-	void search(SearchRequest req, AsyncCallback<SearchResult> callback);
-}

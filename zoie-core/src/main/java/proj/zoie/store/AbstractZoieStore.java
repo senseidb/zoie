67696e6a05b9bd6113c5dbbf51492d410f0fd4ce
John Wang <john.wang@gmail.com>
2011-11-08 19:00:18 -0800

    added store

---
+++ zoie-core/src/main/java/proj/zoie/store/AbstractZoieStore.java
@@ -0,0 +1,135 @@
+package proj.zoie.store;
+
+import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
+import org.apache.log4j.Logger;
+
+import proj.zoie.impl.indexing.ZoieConfig;
+
+public abstract class AbstractZoieStore implements ZoieStore {
+
+	private static Logger logger = Logger.getLogger(AbstractZoieStore.class);
+	private boolean _dataCompressed;
+	private Comparator<String> _versionComparator = ZoieConfig.DEFAULT_VERSION_COMPARATOR;
+
+	private volatile String _version = null;
+	private final Long2ObjectLinkedOpenHashMap<byte[]> _dataMap = new Long2ObjectLinkedOpenHashMap<byte[]>();
+
+	private final ReentrantReadWriteLock _dataMapLock = new ReentrantReadWriteLock();
+	private final WriteLock _writeLock = _dataMapLock.writeLock();
+	private final ReadLock _readLock = _dataMapLock.readLock();
+
+	public void setVersionComparator(Comparator<String> versionComparator) {
+		_versionComparator = versionComparator;
+	}
+
+	public Comparator<String> getVersionComparator() {
+		return _versionComparator;
+	}
+
+	public void setDataCompressed(boolean dataCompressed) {
+		_dataCompressed = dataCompressed;
+	}
+
+	public boolean isDataCompressed() {
+		return _dataCompressed;
+	}
+
+	protected abstract void persist(long uid, byte[] data) throws IOException;
+
+	protected abstract void persistDelete(long uid) throws IOException;
+
+	protected abstract byte[] getFromStore(long uid) throws IOException;
+
+	protected abstract void commitVersion(String version) throws IOException;
+
+	public abstract String getPersistedVersion() throws IOException;
+
+	public abstract void close() throws IOException;
+
+	public void commit() throws IOException {
+		_writeLock.lock();
+		try {
+			commitVersion(_version);
+			_dataMap.clear();
+		} finally {
+			_writeLock.unlock();
+		}
+	}
+
+	@Override
+	public void delete(long uid, String version) throws IOException {
+		_writeLock.lock();
+		try {
+			persistDelete(uid);
+			_dataMap.remove(uid);
+			_version = version;
+		} finally {
+			_writeLock.unlock();
+		}
+	}
+
+	@Override
+	public final void put(long uid, byte[] data, String version)
+			throws IOException {
+		_writeLock.lock();
+		try {
+			persist(uid, data);
+			_dataMap.put(uid, data);
+			_version = version;
+		} finally {
+			_writeLock.unlock();
+
+		}
+	}
+
+	private final byte[] innerGet(long uid) throws IOException {
+		byte[] data = null;
+		try {
+			_readLock.lock();
+			data = _dataMap.get(uid);
+		} finally {
+			_readLock.unlock();
+		}
+		if (data == null) {
+			data = getFromStore(uid);
+		}
+		return data;
+	}
+
+	@Override
+	public final byte[] get(long uid) throws IOException {
+		_readLock.lock();
+		try {
+			return innerGet(uid);
+		} finally {
+			_readLock.unlock();
+		}
+	}
+
+	@Override
+	public final String getVersion() {
+		return _version;
+	}
+
+	@Override
+	public byte[][] get(long[] uids) {
+		byte[][] dataList = new byte[uids.length][];
+		int idx = 0;
+		for (long uid : uids) {
+			try {
+				dataList[idx++] = innerGet(uid);
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
+
+		return dataList;
+	}
+}
---
+++ zoie-core/src/main/java/proj/zoie/store/ZoieStore.java
@@ -0,0 +1,13 @@
+package proj.zoie.store;
+
+import java.io.IOException;
+
+public interface ZoieStore {
+	void put(long uid,byte[] data,String version) throws IOException;
+	byte[] get(long uid) throws IOException;
+	byte[][] get(long[] uids);
+	String getVersion();
+	void delete(long uid,String version) throws IOException;
+	String getPersistedVersion() throws IOException;
+	void close() throws IOException;
+}
